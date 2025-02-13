1a2470b1bd7433dae548cf07432fa0d28aa444d3
LucS0MA <lucien.kohli@gmail.com>
2025-02-13 10:09:27 +0100

    setup backend apollo server

---
+++ backend/src/config/db.ts
@@ -0,0 +1,12 @@
+import { DataSource } from "typeorm";
+
+export const dataHealthCheck = new DataSource({
+  type: "postgres",
+  host: "db",
+  username: "postgres",
+  database: "postgres",
+  password: "example",
+  entities: [],
+  synchronize: true,
+  logging: ["error", "query"],
+});
