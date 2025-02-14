e05c365c80b48e9ee4f5f76e50568033ba9ce92f
Thomas Potherat <t.potherat@laposte.net>
2025-02-14 10:46:37 +0100

    setup shadcn

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+
 import { Outlet } from 'react-router'
+import { Button } from './ui/button'
 
 const Layout = () => {
     return (
         <main>
-
+            <Button>Test</Button>
             <Outlet />
         </main>
     )
