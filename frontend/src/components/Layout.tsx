109e8718fae8fd79d10fa01527a2d69649f38385
LucS0MA <lucien.kohli@gmail.com>
2025-03-06 22:14:08 +0100

    footer

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,14 +1,21 @@
-
-import { Outlet } from 'react-router'
-import { Button } from './ui/button'
+import { Outlet } from 'react-router';
+import { Button } from './ui/button';
+import Footer from './Footer';
 
 const Layout = () => {
     return (
-        <main>
-            <Button>Test</Button>
-            <Outlet />
-        </main>
-    )
-}
+        <div className="flex flex-col min-h-screen">
+            <header className="container mx-auto px-4 py-4">
+                <Button>Test</Button>
+            </header>
 
-export default Layout
+            <main className="flex-grow container mx-auto px-4">
+                <Outlet />
+            </main>
+
+            <Footer />
+        </div>
+    );
+};
+
+export default Layout;
