5c57ae02bead81a0c605f032ff8a5a37ef3567eb
hyna <bylo@duck.com>
2025-03-12 09:19:52 +0100

    feat : setuo e2e test with Docker

---
+++ e2e/tests/basic.test.ts
@@ -0,0 +1,12 @@
+import { test, expect } from "@playwright/test";
+ 
+test("Go to home page", async ({ page }) => {
+   //accèder à la page d'accueil
+   await page.goto("http://api_gateway:80/");
+  
+  //attendre que la page soit complètement chargée
+   await page.waitForLoadState("networkidle");
+  
+  //vérifier que le lien 'See all my scans'
+  await expect(page.getByRole("link", { name: "See all my scans" })).toBeVisible();
+ });
