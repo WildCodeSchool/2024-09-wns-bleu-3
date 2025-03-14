f7c01dd52203311589acd9416b3a83ebaa9f1bf4
Florian <f.rumigny@gmail.com>
2025-03-14 12:13:21 +0100

    second test

--- frontend/src/test/Footer.test.tsx
+++ frontend/src/test/Footer.test.tsx
@@ -1,14 +1,18 @@
 import "@testing-library/jest-dom"; // TODO check if needed
-import { expect, test } from "vitest";
+import { describe, expect, test } from "vitest";
 import { render, screen } from "@testing-library/react";
 import Footer from "../components/Footer";
 import { MemoryRouter } from "react-router";
 
-test("displays footer", async () => {
-    render(
-        <MemoryRouter>
-            <Footer />
-        </MemoryRouter>
-    );
-    expect(await screen.findByText("Terms")).toBeInTheDocument();
-});
+const isDev = import.meta.env.DEV;
+
+describe.runIf(isDev)("footer", () => {
+    test("displays footer", async () => {
+        render(
+            <MemoryRouter>
+                <Footer />
+            </MemoryRouter>
+        );
+        expect(await screen.findByText("Terms")).toBeInTheDocument();
+    });
+})
--- frontend/src/test/FormAddScan.test.tsx
+++ frontend/src/test/FormAddScan.test.tsx
@@ -1,9 +1,10 @@
 import "@testing-library/jest-dom";
 // import userEvent from "@testing-library/user-event";
-import { expect, test, vi } from "vitest";
+import { describe, expect, test, vi } from "vitest";
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import ScanForm from "../components/FormAddScan";
 
+const isDev = import.meta.env.DEV;
 
 const createScanMock = vi.fn(({ onCompleted }) => {
   if (onCompleted) onCompleted();
@@ -25,50 +26,63 @@ vi.mock("../generated/graphql-types", () => ({
   }),
 }));
 
+describe.runIf(isDev)("ScanForm", () => {
 
-test("displays ScanForm", async () => {
-  // const user = userEvent.setup();
-
-  render(<ScanForm />);
-  // screen.debug();
-
-  const titleInput = screen.getByPlaceholderText("My Website Monitor");
-  const urlInput = screen.getByPlaceholderText("https://example.com");
-  const frequencySelect = screen.getByTestId('freqSelectButton');
-  const tagSelect = screen.getByTestId('tagSelectButton');
-  const submitButton = screen.getByRole("button");
-
-  fireEvent.change(titleInput, { target: { value: "youtube" } });
-  fireEvent.change(urlInput, { target: { value: "https://youtube.com" } });
-
-  // To target the select frequence, we need to click on the trigger
-  fireEvent.click(frequencySelect);
-  const vitrineOption = screen.getByRole("option", { name: "Daily" });
-  fireEvent.click(vitrineOption);
-
-  // To target the select tag, we need to click on the trigger
-  fireEvent.click(tagSelect);
-  const tagOption = screen.getByRole("option", { name: "Vitrine" });
-  fireEvent.click(tagOption);
-
-
-  fireEvent.click(submitButton);
-
-  await waitFor(() => {
-    expect(createScanMock).toHaveBeenCalledWith(
-      expect.objectContaining({
-        variables: {
-          data: {
-            title: "youtube",
-            url: "https://youtube.com",
-            tagIds: [1],
-            frequencyId: 1,
+  test("Create a scan", async () => {
+    // const user = userEvent.setup();
+  
+    render(<ScanForm />);
+    // screen.debug();
+  
+    const titleInput = screen.getByPlaceholderText("My Website Monitor");
+    const urlInput = screen.getByPlaceholderText("https://example.com");
+    const frequencySelect = screen.getByTestId('freqSelectButton');
+    const tagSelect = screen.getByTestId('tagSelectButton');
+    const submitButton = screen.getByRole("button");
+  
+    fireEvent.change(titleInput, { target: { value: "youtube" } });
+    fireEvent.change(urlInput, { target: { value: "https://youtube.com" } });
+  
+    // To target the select frequence, we need to click on the trigger
+    fireEvent.click(frequencySelect);
+    const vitrineOption = screen.getByRole("option", { name: "Daily" });
+    fireEvent.click(vitrineOption);
+  
+    // To target the select tag, we need to click on the trigger
+    fireEvent.click(tagSelect);
+    const tagOption = screen.getByRole("option", { name: "Vitrine" });
+    fireEvent.click(tagOption);
+  
+  
+    fireEvent.click(submitButton);
+  
+    await waitFor(() => {
+      expect(createScanMock).toHaveBeenCalledWith(
+        expect.objectContaining({
+          variables: {
+            data: {
+              title: "youtube",
+              url: "https://youtube.com",
+              tagIds: [1],
+              frequencyId: 1,
+            },
           },
-        },
-      })
-    );
-
-    expect(createScanMock).toHaveBeenCalledTimes(1);
-
+        })
+      );
+  
+      expect(createScanMock).toHaveBeenCalledTimes(1);
+  
+    });
   });
+
+  test("Display error message if title or url is empty", async () => {
+    render(<ScanForm />);
+    const submitButton = screen.getByRole("button");
+    fireEvent.click(submitButton);
+    await waitFor(() => {
+      expect(screen.getByText("Le titre est requis")).toBeInTheDocument();
+      expect(screen.getByText("Veuillez entrer une URL valide")).toBeInTheDocument();
+    });
+  });
+
 });
