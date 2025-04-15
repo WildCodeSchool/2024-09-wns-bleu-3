b0f9000a45b0205f53514d645778b07f24bff621
Florian <f.rumigny@gmail.com>
2025-04-15 16:15:24 +0200

    fix introspection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -34,7 +34,7 @@ async function start() {
 
     const server = new ApolloServer({
         schema,
-        introspection: true,
+        introspection: process.env.NODE_ENV === 'production',
     })
 
     /**
