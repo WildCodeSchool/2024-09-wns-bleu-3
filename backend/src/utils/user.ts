68351d110173a709b5a2cef5c38c7f7c61c458ae
LucS0MA <lucien.kohli@gmail.com>
2025-09-24 11:47:41 +0200

    test dynamic link resend

--- backend/src/utils/user.ts
+++ backend/src/utils/user.ts
@@ -1,4 +1,20 @@
+const stagingUrl = "staging.092024-bleu-3.wns.wilders.dev";
+const productionUrl = "092024-bleu-3.wns.wilders.dev";
+const devUrl = "localhost:3030";
+
+function getBaseUrl() {
+  switch (process.env.NODE_ENV) {
+    case "production":
+      return productionUrl;
+    case "staging":
+      return stagingUrl;
+    default:
+      return devUrl
+  }
+}
+
 export function resetPasswordEmail(randomCode: string) {
+  const baseUrl = getBaseUrl();
   return (`
   <div style="background-color: #051525; padding: 20px; font-family: Arial, sans-serif; color: #ffffff; text-align: center;">
       
@@ -14,7 +30,7 @@ export function resetPasswordEmail(randomCode: string) {
       <p style="font-size: 16px;">Vous avez demandé à réinitialiser votre mot de passe.</p>
       <p style="font-size: 16px;">Veuillez cliquer sur le bouton ci-dessous pour le réinitialiser :</p>
       
-      <a href="http://localhost:3030/reset-password?tab=reset&code=${randomCode}" 
+      <a href="http://${baseUrl}/reset-password?tab=reset&code=${randomCode}" 
          style="display: inline-block; padding: 12px 20px; font-size: 16px; 
                 color: #ffffff; background-color: #007bff; text-decoration: none;
                 border-radius: 8px; font-weight: bold; margin: 20px 0;">
@@ -36,7 +52,8 @@ export function resetPasswordEmail(randomCode: string) {
 }
 
 export function registerEmailConfirmation(randomCode: string) {
-    return (`
+  const baseUrl = getBaseUrl();
+  return (`
   <div style="background-color: #051525; padding: 20px; font-family: Arial, sans-serif; color: #ffffff; text-align: center;">
       
       <!-- Logo en texte -->
@@ -51,7 +68,7 @@ export function registerEmailConfirmation(randomCode: string) {
       <p style="font-size: 16px;">Merci de vous être inscrit.</p>
       <p style="font-size: 16px;">Pour activer votre compte veuillez cliquer sur le bouton suivant :</p>
       
-      <a href="http://localhost:3030/register?code=${randomCode}" 
+      <a href="http://${baseUrl}/register?code=${randomCode}" 
          style="display: inline-block; padding: 12px 20px; font-size: 16px; 
                 color: #ffffff; background-color: #007bff; text-decoration: none;
                 border-radius: 8px; font-weight: bold; margin: 20px 0;">
